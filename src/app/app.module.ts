69da85b52c1b231bfda5cd00f722f0a628ad6731
Mayank <mayank_kapoor@live.com>
2021-08-13 00:24:35 +0530

    adding components, services and classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PolicyDetailsComponent } from './components/policy-details/policy-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolicyDetailsComponent
   ],
   imports: [
     BrowserModule,
