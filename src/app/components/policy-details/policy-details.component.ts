eb84cb9c8630b672a15f85e5dafb52bb94ccce74
Kapoor, Mayank <mayank.r.kapoor@medtronic.com>
2021-08-13 17:07:51 +0530

    get and update logic

--- src/app/components/policy-details/policy-details.component.ts
+++ src/app/components/policy-details/policy-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Policy } from 'src/app/models/policy';
+import { PolicyService } from 'src/app/services/policy.service';
 
 @Component({
   selector: 'app-policy-details',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolicyDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private policyServie : PolicyService,
+    private route : ActivatedRoute,
+    private router : Router
+  ) { }
+
+  message = '';
+  currentPolicy : any;
 
   ngOnInit(): void {
+    this.message = '';
+    this.getPolicyById(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getPolicyById(id: any): void {
+    this.policyServie.getById(id)
+    .subscribe(
+      policy => {
+        this.currentPolicy = policy;
+        console.log(policy);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  updatePolicy(): void {
+    this.policyServie.update(this.currentPolicy.id, this.currentPolicy)
+    .subscribe(
+      response => {
+        console.log(response);
+        this.message = 'Policy Updated!'
+      },
+      error =>{
+        console.log(error);
+        this.message = 'Update failed!!';
+      }
+    )
   }
 
 }
